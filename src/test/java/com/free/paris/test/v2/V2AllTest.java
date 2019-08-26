17d4727113299faa630542688fd490eefd55c541
shenxiu <weimin@admindeMacBook-Pro.local>
2019-08-26 14:07:26 +0800

    CustomBoolean\CustomNumber

---
+++ src/test/java/com/free/paris/test/v2/V2AllTest.java
@@ -0,0 +1,13 @@
+package com.free.paris.test.v2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CustomBooleanEditorTest.class,
+        CustomNumberEditorTest.class
+})
+public class V2AllTest {
+
+}
